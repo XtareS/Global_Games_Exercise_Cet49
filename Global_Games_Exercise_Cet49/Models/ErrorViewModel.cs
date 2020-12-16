56bf537880e7e78e403ecddaac63db16859725ec
XtareS <xtare16.soares@gmail.com>
2020-12-16 13:18:57 +0000

    Creation of the Project Global Games

---
+++ Global_Games_Exercise_Cet49/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Global_Games_Exercise_Cet49.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
